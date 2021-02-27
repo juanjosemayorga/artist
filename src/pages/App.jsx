9c6dece2fea51c85d6d246207025160a3eda7a66
Juan Jose Mayorga <tmaniacus@gmail.com>
2021-02-26 18:34:21 -0600

    hero component for mobile ready

---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from './Hero/Hero'
+
+const App = () => (
+  <>
+    <Hero />
+  </>
+  )
+
+export default App
---
+++ src/pages/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => (
+  <div className="hero__container">
+    <h1 className="hero__title">20</h1>
+    <section className="hero__section-container">
+      <h3 className="hero__section-container--year">2020</h3>
+      <h2 className="hero__section-container--text">Your year as an Artlist Creator</h2>
+      <button className="hero__section-container--button" type="button">PLAY</button>
+    </section>
+  </div>
+  )
+
+export default Hero
