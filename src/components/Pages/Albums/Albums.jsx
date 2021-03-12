d88bf5b18c9a909ac4faee1ff549f3516642c23d
Juan Jose Mayorga <tmaniacus@gmail.com>
2021-03-12 12:54:26 -0600

    Albums component for desktop ready

--- src/components/Pages/Albums/Albums.jsx
+++ src/components/Pages/Albums/Albums.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IMAGES_ALBUMS from '@Data/albums.json'
 
 import PLAY_ROUNDED_ICON from '@Assets/icons/play-round.png'
@@ -6,30 +6,62 @@ import PAUSE_ROUNDED_ICON from '@Assets/icons/pause-round.png'
 
 import './Albums.css'
 
-const Albums = () => (
-  <div className="albums-component-container">
-    {
-      IMAGES_ALBUMS.map((album, i) => (
-        <div
-          key={album.id}
-          className={`albums-component__album-container albums-component__album-container--${i}`}
-        >
-          <div className="albums-component__internal-album">
-            <div className='albums-component__internal-album--hover'>
-              <p className={`albums-component__hover--title--${i}`}>{album.name}</p>
-              <img
-                src={PLAY_ROUNDED_ICON}
-                alt={album.name}
-              />
-            </div>
-            <figure className="albums-component__internal-album--figure">
-              <img src={album.link} alt={album.name} />
-            </figure>
-          </div>
-        </div>
-      ))
+const Albums = () => {
+
+  const initialPlaying = {
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+  }
+
+  const [playing, setPlaying] = useState(initialPlaying)
+
+  const handlePlay = (e) => {
+
+    const choosed = e.target.name
+
+    if (playing[choosed]) {
+      setPlaying(initialPlaying)
+    } else {
+      setPlaying({
+        ...initialPlaying,
+        [e.target.name]: true
+      })
     }
-  </div>
-)
+
+  }
+
+  return (
+    <div className="albums-component-container">
+      {
+        IMAGES_ALBUMS.map((album, i) => (
+          <div
+            key={album.id}
+            className={`albums-component__album-container albums-component__album-container--${i}`}
+          >
+            <div className="albums-component__internal-album">
+              <div className='albums-component__internal-album--hover'>
+                <p className={`albums-component__hover--title--${i}`}>{album.name}</p>
+                <input
+                  type="image"
+                  alt={album.name}
+                  src={playing[album.id] ? PAUSE_ROUNDED_ICON : PLAY_ROUNDED_ICON}
+                  name={album.id}
+                  onKeyDown={handlePlay}
+                  onClick={handlePlay}
+                />
+              </div>
+              <figure className="albums-component__internal-album--figure">
+                <img src={album.link} alt={album.name} />
+              </figure>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
 
 export default Albums
